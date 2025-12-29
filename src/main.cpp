3b45982c45cde6430430b27b27ca73628e51db73
kiwataka <kiwataka@yandex.ru>
2025-12-29 05:03:59 +0400

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+class TPMEmulator {
+private:
+    std::vector<uint8_t> ek_cert = {0x01, 0x02, 0x03}; // fake EK
+    uint32_t pcr[24] = {0};
+
+public:
+    void PCR_Extend(int index, const uint8_t* data, size_t len) {
+        if (index < 24) {
+            // Simple hash simulation
+            for (size_t i = 0; i < len; ++i) {
+                pcr[index] = (pcr[index] << 8) ^ data[i];
+            }
+            std::cout << "[TPM] PCR[" << index << "] extended\n";
+        }
+    }
+
+    void GetCapability() {
+        std::cout << "[TPM] GetCapability: TPM 2.0 Emulated\n";
+    }
+};
+
+int main() {
+    TPMEmulator tpm;
+    tpm.GetCapability();
+
+    uint8_t data[] = {0xDE, 0xAD, 0xBE, 0xEF};
+    tpm.PCR_Extend(10, data, sizeof(data));
+
+    std::cout << "TPM 2.0 Emulator running (research only)\n";
+    return 0;
+}
